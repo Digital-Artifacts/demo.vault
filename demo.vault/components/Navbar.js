542b3bb6be08407a891b461f548a3f0b02fda3a3
AdedamolaXL <adedamolaxl@outlook.com>
2023-06-09 01:31:48 +0100

    updated contract code

--- demo.vault/components/Navbar.js
+++ demo.vault/components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav className="flex justify-end bg-white">
-            <button type="button" className="h-16 mt-10 mb-10 mr-20 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Connect wallet</button>
+            <button type="button" className="h-16 mt-10 mb-10 mr-20 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Connect Wallet</button>
         </nav>
     )
 }
